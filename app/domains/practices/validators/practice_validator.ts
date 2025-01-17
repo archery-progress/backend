3d2c469f06a8870021f7b2ee185206f2ec180421
bourgu20al <bourgu20al@lycee-ndduroc.com>
2025-01-17 16:22:42 +0100

    feat: add Practice controller + service + validator

---
+++ app/domains/practices/validators/practice_validator.ts
@@ -0,0 +1,41 @@
+import vine from '@vinejs/vine'
+import { searchComposable } from '#app/commons/validators/searchable'
+import { Infer } from '@vinejs/vine/types'
+
+export const searchPracticeValidator = vine.compile(vine.object(searchComposable.getProperties()))
+
+export const createPracticeValidator = vine.compile(
+  vine.object({
+    userId: vine.number().optional(),
+    structureId: vine.number().optional(),
+    sessionId: vine.number().optional(),
+    name: vine.string().minLength(2).maxLength(255),
+    description: vine.string().minLength(2).maxLength(255),
+    content: vine.string().minLength(2).maxLength(255),
+    metadata: vine.any(),
+    status: vine.string().minLength(2).maxLength(255),
+    results: vine.any(),
+    type: vine.string().minLength(2).maxLength(255),
+  })
+)
+
+export const updatePracticeValidator = vine.compile(
+  vine.object({
+    userId: vine.number().optional(),
+    structureId: vine.number().optional(),
+    sessionId: vine.number().optional(),
+    name: vine.string().minLength(2).maxLength(255).optional(),
+    description: vine.string().minLength(2).maxLength(255).optional(),
+    content: vine.string().minLength(2).maxLength(255).optional(),
+    metadata: vine.any().optional(),
+    status: vine.string().minLength(2).maxLength(255).optional(),
+    results: vine.any().optional(),
+    type: vine.string().minLength(2).maxLength(255).optional(),
+  })
+)
+
+export type SearchPracticeSchema = Infer<typeof searchPracticeValidator>
+export type StorePracticeSchema = Infer<typeof createPracticeValidator>
+export type UpdatePracticeSchema = Infer<typeof updatePracticeValidator> & {
+  uid: string
+}
