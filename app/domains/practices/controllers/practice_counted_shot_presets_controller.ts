68a120d085668d0bcfb6b348b81b429dfafdcfd7
bourgu20al <bourgu20al@lycee-ndduroc.com>
2025-01-17 16:21:18 +0100

    feat: add PracticeCountedShotPreset controller + service + validator

---
+++ app/domains/practices/controllers/practice_counted_shot_presets_controller.ts
@@ -0,0 +1,35 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import {
+  searchPracticeCountedShotPresetValidator,
+  createPracticeCountedShotPresetValidator,
+  updatePracticeCountedShotPresetValidator,
+} from '#domains/practices/validators/practice_counted_shot_preset_validator'
+import { inject } from '@adonisjs/core'
+import PracticeCountedShotPresetService from '#domains/practices/services/practice_counted_shot_preset_service'
+
+@inject()
+export default class PracticeCountedShotPresetsController {
+  constructor(private practiceCountedShotPresetService: PracticeCountedShotPresetService) {}
+
+  async index({ request }: HttpContext) {
+    const payload = await request.validateUsing(searchPracticeCountedShotPresetValidator)
+    return this.practiceCountedShotPresetService.paginate(payload)
+  }
+
+  async store({ request }: HttpContext) {
+    const payload = await request.validateUsing(createPracticeCountedShotPresetValidator)
+    return this.practiceCountedShotPresetService.store(payload)
+  }
+
+  async update({ params, request }: HttpContext) {
+    const payload = await request.validateUsing(updatePracticeCountedShotPresetValidator)
+    return this.practiceCountedShotPresetService.update({
+      ...payload,
+      uid: params.uid,
+    })
+  }
+
+  async destroy({ params }: HttpContext) {
+    return await this.practiceCountedShotPresetService.delete(params.uid)
+  }
+}
---
+++ app/domains/practices/validators/practice_counted_shot_preset_validator.ts
@@ -0,0 +1,41 @@
+import vine from '@vinejs/vine'
+import { searchComposable } from '#app/commons/validators/searchable'
+import { Infer } from '@vinejs/vine/types'
+
+export const searchPracticeCountedShotPresetValidator = vine.compile(
+  vine.object(searchComposable.getProperties())
+)
+
+export const createPracticeCountedShotPresetValidator = vine.compile(
+  vine.object({
+    name: vine.string().minLength(2).maxLength(255),
+    description: vine.string().minLength(2).maxLength(255),
+    content: vine.string().minLength(2).maxLength(255),
+    metadata: vine.any(),
+    flags: vine.any(),
+    type: vine.string().minLength(2).maxLength(255),
+    structureId: vine.number().optional(),
+  })
+)
+
+export const updatePracticeCountedShotPresetValidator = vine.compile(
+  vine.object({
+    name: vine.string().minLength(2).maxLength(255).optional(),
+    description: vine.string().minLength(2).maxLength(255).optional(),
+    content: vine.string().minLength(2).maxLength(255).optional(),
+    metadata: vine.any().optional(),
+    flags: vine.any().optional(),
+    type: vine.string().minLength(2).maxLength(255).optional(),
+    structureId: vine.number().optional(),
+  })
+)
+
+export type SearchPracticeCountedShotPresetSchema = Infer<
+  typeof searchPracticeCountedShotPresetValidator
+>
+export type CreatePracticeCountedShotPresetSchema = Infer<
+  typeof createPracticeCountedShotPresetValidator
+>
+export type UpdatePracticeCountedShotPresetSchema = Infer<
+  typeof updatePracticeCountedShotPresetValidator
+> & { uid: string }
