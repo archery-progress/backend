a0c7c57e2ec903eab2f3149f91ff87668d1fe197
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-20 14:17:47 +0100

    feat: refacto migrations

--- database/migrations/1732378629907_create_roles_table.ts
+++ database/migrations/1732378629907_create_roles_table.ts
@@ -5,11 +5,11 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.bigIncrements('id').primary()
-      table.string('uid').notNullable().unique()
+      table.string('id').primary()
       table.string('name').notNullable()
       table.string('description').nullable()
-      table.boolean('for_admin').notNullable()
+
+      table.integer('permissions').notNullable()
       table.timestamp('created_at')
       table.timestamp('updated_at')
     })
