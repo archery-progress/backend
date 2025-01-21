ce2c452717421c2f30d5a60e3c5c7e539a20ee1f
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-21 16:43:37 +0100

    test: write test

--- tests/functional/roles/create.spec.ts
+++ tests/functional/roles/create.spec.ts
@@ -1,31 +1,19 @@
 import { test } from '@japa/runner'
-import app from '@adonisjs/core/services/app'
-import RoleService from '#domains/roles/services/role_service'
 import { Permissions } from '#app/commons/services/permission_service'
 import { UserFactory } from '#database/factories/user_factory'
 import { UserStatus, UserType } from '#models/user'
+import { StructureFactory } from '#database/factories/structure_factory'
+import { MemberFactory } from '#database/factories/member_factory'
+import { RoleFactory } from '#database/factories/role_factory'
 
 test.group('Roles create', () => {
-  test('example test', async ({ assert }) => {
-    const roleService = await app.container.make(RoleService)
-
-    const permissions = Permissions.MANAGE_MEMBERS | Permissions.VIEW_LOGS
-
-    const role = await roleService.create({
-      name: 'Admin',
-      permissions,
-    })
-
-    console.log(role.id)
-
-    assert.equal(role.permissions & Permissions.MANAGE_MEMBERS, Permissions.MANAGE_MEMBERS)
-    assert.equal(role.permissions & Permissions.VIEW_LOGS, Permissions.VIEW_LOGS)
-  })
-
-  test('retourner 201 si le role est créé avec succès', async ({ client }) => {
+  test('must return 201 if the structure owner creates a role', async ({ client }) => {
     const user = await UserFactory(UserType.staff, UserStatus.verified).make()
+    const structure = await StructureFactory(user).make()
+    await MemberFactory(user, structure).make()
+
     const response = await client
-      .post('/v1/structures/1/roles')
+      .post(`/v1/structures/${structure.id}/roles`)
       .json({
         name: 'Admin',
         permissions: Permissions.MANAGE_MEMBERS | Permissions.VIEW_LOGS,
@@ -34,7 +22,66 @@ test.group('Roles create', () => {
       .send()
 
     response.assertStatus(201)
+  })
 
-    console.log(response.body())
+  test('must return 201 if an administrator creates a role', async ({ client }) => {
+    const user = await UserFactory(UserType.staff, UserStatus.verified).make()
+    const structure = await StructureFactory().make()
+    const member = await MemberFactory(user, structure).make()
+    await RoleFactory(structure, [Permissions.ADMINISTRATOR], member).make()
+
+    const response = await client
+      .post(`/v1/structures/${structure.id}/roles`)
+      .json({
+        name: 'Admin',
+        permissions: Permissions.MANAGE_MEMBERS | Permissions.VIEW_LOGS,
+      })
+      .loginAs(user)
+      .send()
+
+    response.assertStatus(201)
+  })
+
+  test('should return 201 if a user with the right permissions creates a role', async ({
+    client,
+  }) => {
+    const user = await UserFactory(UserType.staff, UserStatus.verified).make()
+    const structure = await StructureFactory().make()
+    const member = await MemberFactory(user, structure).make()
+    await RoleFactory(structure, [Permissions.MANAGE_ROLES], member).make()
+
+    const response = await client
+      .post(`/v1/structures/${structure.id}/roles`)
+      .json({
+        name: 'Admin',
+        permissions: Permissions.MANAGE_MEMBERS | Permissions.VIEW_LOGS,
+      })
+      .loginAs(user)
+      .send()
+
+    response.assertStatus(201)
+  })
+
+  test('must return 403 if a user without the right permissions creates a role', async ({ client }) => {
+    const user = await UserFactory(UserType.staff, UserStatus.verified).make()
+    const structure = await StructureFactory().make()
+    await MemberFactory(user, structure).make()
+
+    const response = await client
+      .post(`/v1/structures/${structure.id}/roles`)
+      .json({
+        name: 'Admin',
+        permissions: Permissions.MANAGE_MEMBERS | Permissions.VIEW_LOGS,
+      })
+      .loginAs(user)
+      .send()
+
+    response.assertStatus(403)
+  })
+
+  test('must return 401 if an unauthenticated user creates a role', async ({ client }) => {
+    const response = await client.post(`/v1/structures/1/roles`).json({}).send()
+
+    response.assertStatus(401)
   })
 })
