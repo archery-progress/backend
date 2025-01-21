57928fded5e9f2632668499f0638ac3b62a21a80
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-21 16:43:53 +0100

    feat: update migrations & factories

--- database/factories/structure_factory.ts
+++ database/factories/structure_factory.ts
@@ -1,12 +1,13 @@
 import factory from '@adonisjs/lucid/factories'
 import Structure from '#models/structure'
 import User from '#models/user'
+import { UserFactory } from '#database/factories/user_factory'
 
 export function StructureFactory(owner?: User) {
   return factory
     .define(Structure, async ({ faker }) => {
       return Structure.create({
-        ownerId: owner?.id ?? undefined,
+        ownerId: owner?.id ?? (await UserFactory().make()).id,
         name: faker.company.name(),
         siret: faker.string.numeric(14),
         isDeactivated: faker.datatype.boolean(),
--- database/migrations/1737462992066_create_members_table.ts
+++ database/migrations/1737462992066_create_members_table.ts
@@ -1,14 +1,14 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'roles'
+  protected tableName = 'members'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.string('id').primary()
-      table.string('name').notNullable()
-      table.string('structure_id').references('id').inTable('structures').notNullable().onDelete('CASCADE')
-      table.integer('permissions').notNullable()
+
+      table.string('user_id').references('id').inTable('users').notNullable()
+      table.string('structure_id').references('id').inTable('structures').notNullable()
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
