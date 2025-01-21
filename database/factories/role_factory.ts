dce66896d9693cc00fc85f69c7d53ee99a279f2d
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-21 16:44:00 +0100

    feat: update migrations & factories

--- database/factories/role_factory.ts
+++ database/factories/role_factory.ts
@@ -1,12 +1,24 @@
+import Structure from '#models/structure'
+import { Permissions } from '#app/commons/services/permission_service'
+import Member from '#models/member'
 import factory from '@adonisjs/lucid/factories'
 import Role from '#models/role'
 
-export function RoleFactory() {
+export function RoleFactory(structure: Structure, permissions: Permissions[], member?: Member) {
   return factory
-    .define(Role, async ({ faker }) => {
-      return Role.create({
-        name: faker.word.adjective(),
+    .define(Role, async ({}) => {
+      const bitfield = permissions.reduce((acc, permission) => acc | permission, 0)
+      const role = await Role.create({
+        structureId: structure.id,
+        name: 'role',
+        permissions: bitfield,
       })
+
+      if (member) {
+        await role.related('members').attach([member.id])
+      }
+
+      return role
     })
     .build()
 }
