3d26cc6208932a1fa5b4a2aa5c90ea2d9bc51f57
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-21 16:44:56 +0100

    feat: Add structureId to role creation in RolesController

--- app/domains/roles/controllers/roles_controller.ts
+++ app/domains/roles/controllers/roles_controller.ts
@@ -2,6 +2,8 @@ import { inject } from '@adonisjs/core'
 import RoleService from '#domains/roles/services/role_service'
 import { HttpContext } from '@adonisjs/core/http'
 import { createRoleValidator } from '#domains/roles/validators/role_validators'
+import logger from '@adonisjs/core/services/logger'
+import RolePolicy from '#domains/roles/policies/role_policy'
 
 @inject()
 export default class RolesController {
@@ -10,10 +12,16 @@ export default class RolesController {
   async index({}: HttpContext) {}
   async show({}: HttpContext) {}
 
-  async store({ request, response }: HttpContext) {
+  async store({ request, response, params, bouncer }: HttpContext) {
+    const { structureId } = params
+    await bouncer.with(RolePolicy).authorize('create' as never, structureId)
     const data = await request.validateUsing(createRoleValidator)
 
-    const role = await this.roleService.create(data)
+    const role = await this.roleService.create({
+      ...data,
+      structureId,
+    })
+    logger.info(`Role created: ${role.id} to structure: ${structureId}`)
 
     return response.created(role)
   }
