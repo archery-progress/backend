344735d54764f62d7d7c00e449cf3bbeff0bc268
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-22 19:31:36 +0100

    feat: Add create method to MembersController for creating new members

--- app/domains/members/services/member_service.ts
+++ app/domains/members/services/member_service.ts
@@ -1,6 +1,6 @@
 import Structure from '#app/commons/models/structure'
 import Member from '#models/member'
-import { GetMembersSchema } from '#domains/members/validators/member_validator'
+import { CreateMemberSchema, GetMembersSchema } from '#domains/members/validators/member_validator'
 
 export default class MemberService {
   async findFromStructure(structureId: string, userId: string): Promise<Member> {
@@ -21,5 +21,17 @@ export default class MemberService {
       .andWhereNot('user_id', ownerId)
       .preload('roles')
       .paginate(page, limit)
+  }
+
+  async findById(memberId: string) {
+    return Member.query().where('id', memberId).firstOrFail()
+  }
+
+  async create({ structureId, userId }: CreateMemberSchema) {
+    return Member.create({
+      structureId,
+      userId,
+      permissions: 0,
+    })
   }
 }
