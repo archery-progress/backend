00beebc0e133ff2fb312318e539627b20323cd1f
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-22 13:15:48 +0100

    feat: Add findAllByStructureId method to MemberService

--- app/domains/members/services/member_service.ts
+++ app/domains/members/services/member_service.ts
@@ -1,4 +1,6 @@
+import Structure from '#app/commons/models/structure'
 import Member from '#models/member'
+import { GetMembersSchema } from '#domains/members/validators/member_validator'
 
 export default class MemberService {
   async findFromStructure(structureId: string, userId: string): Promise<Member> {
@@ -7,5 +9,17 @@ export default class MemberService {
       .andWhere('user_id', userId)
       .preload('roles')
       .firstOrFail()
+  }
+
+  async findAllByStructureId({ structureId, limit = 50, page = 1 }: GetMembersSchema) {
+    const structure = await Structure.findOrFail(structureId)
+
+    const ownerId = structure.ownerId
+
+    return Member.query()
+      .where('structure_id', structureId)
+      .andWhereNot('user_id', ownerId)
+      .preload('roles')
+      .paginate(page, limit)
   }
 }
