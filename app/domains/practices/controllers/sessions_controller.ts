33eb411950af676e994e7003e274ef22702970bb
bourgu20al <bourgu20al@lycee-ndduroc.com>
2025-01-23 12:37:28 +0100

    feat: add session policy and update controller

--- app/domains/practices/controllers/sessions_controller.ts
+++ app/domains/practices/controllers/sessions_controller.ts
@@ -6,30 +6,43 @@ import {
 } from '#domains/practices/validators/session_validator'
 import SessionService from '#domains/practices/services/session_service'
 import { inject } from '@adonisjs/core'
+import SessionPolicy from '#domains/practices/policies/sessions_policy'
 
 @inject()
 export default class SessionsController {
   constructor(private sessionService: SessionService) {}
 
-  async index({ request }: HttpContext) {
+  async index({ request, bouncer, params }: HttpContext) {
+    const { structureId } = params
     const payload = await request.validateUsing(searchSessionValidator)
+    await bouncer.with(SessionPolicy).authorize('view', structureId)
+
     return this.sessionService.paginate(payload)
   }
 
-  async store({ request }: HttpContext) {
+  async store({ request, params, bouncer }: HttpContext) {
+    const { structureId } = params
     const payload = await request.validateUsing(storeSessionValidator)
+    await bouncer.with(SessionPolicy).authorize('create', structureId)
+
     return this.sessionService.store(payload)
   }
 
-  async update({ params, request }: HttpContext) {
+  async update({ params, request, bouncer }: HttpContext) {
+    const { structureId } = params
     const payload = await request.validateUsing(updateSessionValidator)
+    await bouncer.with(SessionPolicy).authorize('update', structureId)
+
     return this.sessionService.update({
       ...payload,
       uid: params.uid,
     })
   }
 
-  async destroy({ params }: HttpContext) {
+  async destroy({ params, bouncer }: HttpContext) {
+    const { structureId } = params
+    await bouncer.with(SessionPolicy).authorize('delete', structureId)
+
     return this.sessionService.delete(params.uid)
   }
 }
