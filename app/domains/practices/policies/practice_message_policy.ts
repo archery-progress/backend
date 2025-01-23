e5b9a5cc7ffe20c158962ee655b505f826749eed
bourgu20al <bourgu20al@lycee-ndduroc.com>
2025-01-23 16:23:25 +0100

    feat: create policies for all practices

---
+++ app/domains/practices/policies/practice_message_policy.ts
@@ -0,0 +1,53 @@
+import { BasePolicy } from '@adonisjs/bouncer'
+import { inject } from '@adonisjs/core'
+import { Permissions, PermissionService } from '#app/commons/services/permission_service'
+import User from '#models/user'
+
+@inject()
+export default class PracticeMessagePolicy extends BasePolicy {
+  constructor(protected permissionService: PermissionService) {
+    super()
+  }
+
+  async before(user: User, _action: string, structureId: string) {
+    await user.load('structures')
+    const structures = user.structures
+
+    if (
+      await this.permissionService.hasSomePermissions(user.id, structureId, [
+        Permissions.ADMINISTRATOR,
+        Permissions.MANAGE_STRUCTURE,
+      ])
+    ) {
+      return true
+    }
+
+    if (structures.some((structure) => structure.id === structureId)) {
+      return true
+    }
+  }
+
+  async view(user: User, structureId: string) {
+    return this.permissionService.hasSomePermissions(user.id, structureId, [
+      Permissions.MANAGE_PRACTICES,
+    ])
+  }
+
+  async create(user: User, structureId: string) {
+    return this.permissionService.hasSomePermissions(user.id, structureId, [
+      Permissions.MANAGE_PRACTICES,
+    ])
+  }
+
+  async update(user: User, structureId: string) {
+    return this.permissionService.hasSomePermissions(user.id, structureId, [
+      Permissions.MANAGE_PRACTICES,
+    ])
+  }
+
+  async delete(user: User, structureId: string) {
+    return this.permissionService.hasSomePermissions(user.id, structureId, [
+      Permissions.MANAGE_PRACTICES,
+    ])
+  }
+}
