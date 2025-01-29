3504a0a5698fcb611d282952e796c8eee943a5f9
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-29 02:50:45 +0100

    refactor: Update MemberFactory to support permissions in Member creation

--- database/factories/member_factory.ts
+++ database/factories/member_factory.ts
@@ -2,14 +2,15 @@ import User from '#models/user'
 import Structure from '#models/structure'
 import factory from '@adonisjs/lucid/factories'
 import Member from '#models/member'
+import { Permissions } from '#app/commons/services/permission_service'
 
-export function MemberFactory(user: User, structure: Structure) {
+export function MemberFactory(user: User, structure: Structure, permissions?: Permissions[]) {
   return factory
     .define(Member, async ({}) => {
       return Member.create({
         userId: user.id,
         structureId: structure.id,
-        permissions: 0,
+        permissions: permissions ? permissions.reduce((acc, permission) => permission | acc, 0) : 0,
       })
     })
     .build()
