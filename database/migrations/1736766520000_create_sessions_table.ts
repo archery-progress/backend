06e2a55e3aedb66641c2ceb1eb9f7d92826362dd
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2025-01-31 16:04:34 +0100

    feat: change session nullable columns

--- database/migrations/1736766520000_create_sessions_table.ts
+++ database/migrations/1736766520000_create_sessions_table.ts
@@ -6,10 +6,11 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.string('id').primary()
+      table.string('name').nullable()
+      table.string('description').nullable()
       table.string('structure_id').references('id').inTable('structures').nullable()
-      table.timestamp('target_datetime').notNullable()
+      table.timestamp('target_datetime').nullable()
       table.jsonb('order').notNullable()
-      table.string('description').notNullable()
       table.timestamp('created_at')
       table.timestamp('updated_at')
     })
