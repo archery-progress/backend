cfa11bdc3c3f19238e3f5ce5af3ced0eb8dfcd5d
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-02-13 14:04:23 +0100

    feat: Add endpoint to retrieve user's structures

--- app/domains/accounts/routes.ts
+++ app/domains/accounts/routes.ts
@@ -2,9 +2,15 @@ import router from '@adonisjs/core/services/router'
 import { middleware } from '#start/kernel'
 
 const UsersController = () => import('#domains/accounts/controllers/users_controller')
-
+const UserStructuresController = () =>
+  import('#domains/accounts/controllers/user_structures_controller')
 router
   .group(() => {
+    router
+      .group(() => {
+        router.get('/structures', [UserStructuresController, 'index'])
+      })
+      .prefix('/users/@me')
     router
       .group(() => {
         router.get('', [UsersController, 'index']).as('index')
@@ -19,4 +25,3 @@ router
   })
   .prefix('v1')
   .middleware(middleware.auth())
-  .as('v1')
